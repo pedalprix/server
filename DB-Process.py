ef01ef01164a37f137fb5456fe8a728ddb42e4d7
Steve <wisefuture0@gmail.com>
2016-09-04 22:59:42 +0930

    working

--- DB-Process.py
+++ DB-Process.py
@@ -11,9 +11,13 @@
 import MySQLdb
 import json
 import sys
+import time
 import math
 
 configfilename = "Log-config.json"
+
+def beep():
+    print "\a"
 
 def is_json(myjson):
   try:
@@ -37,8 +41,8 @@ SQL_DB = json_data['SQL_DB']
 
 # A*A + B*B = C*C is formula for circle where A and B are othagonal and C is radius.
 def DistanceBetween(Location1, Location2):
-   diffLongitude = Location1[0] - Location2[0]
-   diffLatitude = Location1[1] - Location2[1]
+   diffLatitude = Location1[0] - Location2[0]
+   diffLongitude = Location1[1] - Location2[1]
    distance = math.sqrt((diffLongitude*diffLongitude)+(diffLatitude*diffLatitude))
    return distance
 
@@ -68,41 +72,57 @@ def SendToSQL(SQLcmd):
    return status
 
 # THESE NUMBERS NEED TO BE RECHECKED ON SITE FOR MB = Murray Bridge
-# GPS Co-Ordinates are (Longitude, Latitude)
-GPS_Finish_Line =         (-35.121015, 139.284525)
+# GPS Co-Ordinates are (Latitude, Longitude)
+GPS_Finish_Line =         (-34.929986, 138.620138)
 GPS_Pit_Lane_Start =      (-35.123919, 139.286669)
 GPS_Pit_Lane_End =        (-35.123024, 139.286734)
 GPS_Pits =                (-35.123620, 139.286670)
 
+Start_100Meters = (-34.929214, 138.577813)
+Finish_100Meters = (-34.929916, 138.578308)
+_105m = 0.000859
+_1m = _105m / 105.0
+
+Limit_Distance = 30.0
 
 while True:
   # Get all rows from GPS_Log where D_Proc_Flag = FALSE
-  sql = "SELECT entry_id, Car_Longitude, Car_Latitude \
+  sql = "SELECT entry_id, Car_Latitude, Car_Longitude, Car_Speed \
          FROM GPS_Log \
-         WHERE D_Proc_Flag=FALSE;"
+         WHERE D_Proc_Flag=0;"
   cursor.execute(sql)
   if cursor.rowcount: # Something to do
     rows = cursor.fetchall()
     for row in rows:
       entry_id = row[0]
       Car_Location = (row[1],row[2])
-
+      Car_Speed = row[3]
       D_Finish_Line = DistanceBetween(GPS_Finish_Line, Car_Location)
       D_Pit_Lane_Start = DistanceBetween(GPS_Pit_Lane_Start, Car_Location)
       D_Pit_Lane_End = DistanceBetween(GPS_Pit_Lane_End, Car_Location)
       D_Pits = DistanceBetween(GPS_Pits, Car_Location)
-      
+
+      D_Finish_Line = D_Finish_Line / _1m
+      D_Pit_Lane_Start = D_Pit_Lane_Start / _1m
+      D_Pit_Lane_End = D_Pit_Lane_End / _1m
+      D_Pits = D_Pits/ _1m
+
+#      astring = "D_Finish_Line :" + str(D_Finish_Line) + "  Limit_Distance " + str(Limit_Distance)
+#      print astring
+
       sql = "UPDATE GPS_Log \
              SET D_Proc_Flag=TRUE, \
                  D_Finish_Line=" + str(D_Finish_Line) + ", \
                  D_Pit_Lane_Start=" + str(D_Pit_Lane_Start) + ", \
                  D_Pit_Lane_End=" + str(D_Pit_Lane_End) + ", \
-                 D_Pits=" + str(D_Finish_Line) + " \
+                 D_Pits=" + str(D_Pits) + " \
              WHERE entry_id=" + str(entry_id) + ";"
-      print "SQL : ", sql
+#      print "SQL : ", sql
       SendToSQL(sql)
   else: # No unprocessed rows
+    print "No unprocessed rows"
     pass
+  time.sleep(2)
 
 
 
